5e8f9b9a8807b45bcb8da09f239b51f42dabba12
Katelyn FitzGerald <7872563+kafitzgerald@users.noreply.github.com>
2025-12-09 16:20:02 -0700

    Replace pkg_resources and bump Python version requirements (#203)
    * replace pkg_resources
    
    * replace pkg_resources use in datasets.py
    
    * Update setup.cfg
    
    * fix typo
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update Python versions
    
    * group Dependabot updates
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pythia_datasets/__init__.py
+++ pythia_datasets/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # flake8: noqa
 """Top-level module for pythia-datasets ."""
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import version as _version
 
 from .datasets import DATASETS, locate
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: no cover
+    __version__ = _version(__name__)
+except Exception:  # pragma: no cover
     # package is not installed
     __version__ = 'unknown'  # pragma: no cover
